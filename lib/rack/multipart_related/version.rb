4719fb9885a0b7af4ff82c33a7f3253d8b3f62a3
Marcelo Manzan <manzan@gmail.com>
2010-12-20 23:41:22 -0200

    refactoring on version.rb

--- lib/rack/multipart_related/version.rb
+++ lib/rack/multipart_related/version.rb
@@ -1,24 +1,12 @@
 module Rack
   class MultipartRelated
-    path = ::File.expand_path('../../..', __FILE__)
-    
     version = nil
-    version = $1 if path =~ /\/rack-multipart_related-([\w\.\-]+)/
-    
-    if version.nil?
-      path = ::File.expand_path('../../../../.git', __FILE__)
-      if ::File.exists?(path)
-        require "step-up"
-        version = StepUp::Driver::Git.last_version
-      end
+    version = $1 if ::File.expand_path('../../..', __FILE__) =~ /\/rack-multipart_related-([\w\.\-]+)/
+    if version.nil? && ::File.exists?(::File.expand_path('../../../../.git', __FILE__))
+      require "step-up"
+      version = StepUp::Driver::Git.last_version
     end
-    
-    if version.nil?
-      VERSION = "0.0.0"
-    else
-      version.sub!(/^v/, '')
-      version.sub!(/\+\d*$/, '')
-      VERSION = version
-    end
+    version = "0.0.0" if version.nil?
+    VERSION = version.gsub(/^v?([^\+]+)\+?\d*$/, '\1')
   end
 end
